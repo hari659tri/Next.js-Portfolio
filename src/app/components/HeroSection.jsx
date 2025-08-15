53927345c968a5315c0ba596d729aa14a9483ea5
hari659tri <harikesht845@gmail.com>
2025-08-15 14:22:43 +0530

    Force update image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -71,13 +71,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/hero.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
-              width={350}
-              height={300}
+              width={400}
+              height={400}
             />
           </div>
         </motion.div>
